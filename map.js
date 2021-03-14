93f1fb10d662bcddbd8e98064c2b9a8485275665
greg-g-dev <geeper@protonmail.com>
2021-03-14 15:57:19 -0700

    Added logic to set state color to party color of winner when you mouse over

--- map.js
+++ map.js
@@ -1,78 +1,4 @@
-/* My Politician Stuff */
-var createPolitician = function (name,partyColor)
-{
-  var politician = {};
-  politician.name = name;
-  politician.electionResults = null;
-  politician.totalVotes = 0;
-  politician.partyColor = partyColor;
 
-  politician.setStateResults = function (state)
-  {
-    if (candidate1.electionResults[state] > candidate2.electionResults[state] )
-      {
-        console.log(state + " = " + candidate1.name);
-      }
-    else if (candidate1.electionResults[state]  < candidate2.electionResults[state] )
-      {
-        console.log(state + " = " + candidate2.name);
-      }
-    else
-      {
-        console.log(state + " = " + "a draw");
-      }
-  };
-
-  politician.countVotes = function()
-  {
-
-    for (var i = 0; i < this.electionResults.length; i++)
-    {
-      this.totalVotes += this.electionResults[i];
-    }
-
-  };
-
-
-  return politician;
-};
-
-/* Create Politician Objects */
-var candidate1 = createPolitician("Bob","rgb[132, 17, 11]");
-var candidate2 = createPolitician("Jane","rgb[245, 141, 136]");
-
-/* Populate election results */
-candidate1.electionResults = [5,1,7,2,17,6,4,2,1,1,8,3,1,11,11,0,5,3,3,3,7,4,8,9,3,7,2,2,4,2,8,3,15,15,2,12,0,4,13,1,3,2,8,11,3,2,11,1,3,7,2];
-candidate2.electionResults = [4,2,4,4,38,3,3,1,2,28,8,1,3,9,0,6,1,5,5,1,3,7,8,1,3,3,1,3,2,2,6,2,14,0,1,6,7,3,7,3,6,1,3,27,3,1,2,11,2,3,1];
-
-/* Count total votes */
-candidate1.countVotes();
-candidate2.countVotes();
-
-var winner;
-
-if (candidate1.totalVotes > candidate2.totalVotes)
-  {
-    winner = candidate1.name;
-  }
-else if (candidate1.totalVotes < candidate2.totalVotes)
-  {
-    winner = candidate2.name;
-  }
-else
-  {
-    winner = "a draw";
-  }
-
-
-/* Display total votes in console */
-console.log("Total votes for " + candidate1.name + " = " + candidate1.totalVotes);
-console.log("Party color for " + candidate1.name + " = " + candidate1.partyColor);
-console.log("Total votes for " + candidate2.name + " = " + candidate2.totalVotes);
-console.log("Party color for " + candidate2.name + " = " + candidate2.partyColor);
-
-console.log("Winner is " + winner);
-/* End My Stuff */
 
 /* Map Technology Copyright (C) 2011 DougX.net, used with permission */
 
@@ -1064,3 +990,86 @@ var inittheStates = function () {
 var onLoad = function () {
   initMap();
 };
+
+/* My Politician Stuff */
+var createPolitician = function (name,partyColor)
+{
+  var politician = {};
+  politician.name = name;
+  politician.electionResults = null;
+  politician.totalVotes = 0;
+  politician.partyColor = partyColor;
+
+  politician.countVotes = function()
+  {
+
+    for (var i = 0; i < this.electionResults.length; i++)
+    {
+      this.totalVotes += this.electionResults[i];
+    }
+
+  };
+
+
+  return politician;
+};
+
+/* Create Politician Objects */
+var candidate1 = createPolitician("Bob",[132, 17, 11]);
+var candidate2 = createPolitician("Jane",[245, 141, 136]);
+
+/* Populate election results */
+candidate1.electionResults = [5,1,7,2,17,6,4,2,1,1,8,3,1,11,11,0,5,3,3,3,7,4,8,9,3,7,2,2,4,2,8,3,15,15,2,12,0,4,13,1,3,2,8,11,3,2,11,1,3,7,2];
+candidate2.electionResults = [4,2,4,4,38,3,3,1,2,28,8,1,3,9,0,6,1,5,5,1,3,7,8,1,3,3,1,3,2,2,6,2,14,0,1,6,7,3,7,3,6,1,3,27,3,1,2,11,2,3,1];
+
+/* Set State rgb color to Party Winner */
+var setStateResults = function (state)
+{
+  var stateWinner;
+  if (candidate1.electionResults[state] > candidate2.electionResults[state] )
+    {
+      stateWinner = candidate1.partyColor;
+    }
+  else if (candidate1.electionResults[state]  < candidate2.electionResults[state] )
+    {
+      stateWinner = candidate2.partyColor;
+    }
+  else
+    {
+      stateWinner = [11, 32, 57];
+    }
+
+    console.log(stateWinner);
+    theStates[state].rgbColor = stateWinner;
+
+};
+
+
+/* Count total votes */
+candidate1.countVotes();
+candidate2.countVotes();
+
+var winner;
+
+if (candidate1.totalVotes > candidate2.totalVotes)
+  {
+    winner = candidate1.name;
+  }
+else if (candidate1.totalVotes < candidate2.totalVotes)
+  {
+    winner = candidate2.name;
+  }
+else
+  {
+    winner = "a draw";
+  }
+
+
+/* Display total votes in console */
+console.log("Total votes for " + candidate1.name + " = " + candidate1.totalVotes);
+console.log("Party color for " + candidate1.name + " = " + candidate1.partyColor);
+console.log("Total votes for " + candidate2.name + " = " + candidate2.totalVotes);
+console.log("Party color for " + candidate2.name + " = " + candidate2.partyColor);
+
+console.log("Winner is " + winner);
+/* End My Stuff */
