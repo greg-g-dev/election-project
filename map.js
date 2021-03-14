0308f138df134805e90465915ee7608ce01a334e
greg-g-dev <geeper@protonmail.com>
2021-03-14 14:11:20 -0700

    Added method to calculate total votes

--- map.js
+++ map.js
@@ -6,16 +6,35 @@ var createPolitician = function (name)
   politician.electionResults = null;
   politician.totalVotes = 0;
 
+  politician.countVotes = function()
+  {
+
+    for (var i = 0; i < this.electionResults.length; i++)
+    {
+      this.totalVotes += this.electionResults[i];
+    }
+
+  };
+
+
   return politician;
 };
 
-
+/* Create Politician Objects */
 var candidate1 = createPolitician("Bob");
 var candidate2 = createPolitician("Jane");
+
+/* Populate election results */
 candidate1.electionResults = [5,1,7,2,17,6,4,2,1,1,8,3,1,11,11,0,5,3,3,3,7,4,8,9,3,7,2,2,4,2,8,3,15,15,2,12,0,4,13,1,3,2,8,11,3,2,11,1,3,7,2];
 candidate2.electionResults = [4,2,4,4,38,3,3,1,2,28,8,1,3,9,0,6,1,5,5,1,3,7,8,1,3,3,1,3,2,2,6,2,14,0,1,6,7,3,7,3,6,1,3,27,3,1,2,11,2,3,1];
-console.log(candidate1);
-console.log(candidate2);
+
+/* Count total votes */
+candidate1.countVotes();
+candidate2.countVotes();
+
+/* Display total votes in console */
+console.log("Total votes for " + candidate1.name + " = " + candidate1.totalVotes);
+console.log("Total votes for " + candidate2.name + " = " + candidate2.totalVotes);
 /* End My Stuff */
 
 /* Map Technology Copyright (C) 2011 DougX.net, used with permission */
