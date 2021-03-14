dd82cdd960f0a21eb3bbdabda59ce1df96a1b365
greg-g-dev <geeper@protonmail.com>
2021-03-14 08:48:18 -0700

    Update candidate election results

--- map.js
+++ map.js
@@ -12,8 +12,8 @@ var createPolitician = function (name)
 
 var candidate1 = createPolitician("Bob");
 var candidate2 = createPolitician("Jane");
-candidate1.electionResults = [5,1,7,2,33,6,4,2,1,14,8,3,1,11,11,0,5,3,3,3,7,4,8,9,3,7,2,2,4,2,8,3,15,15,2,12,0,4,13,1,3,2,8,21,3,2,11,1,3,7,2];
-candidate2.electionResults = [4,2,4,4,22,3,3,1,2,15,8,1,3,9,0,6,1,5,5,1,3,7,8,1,3,3,1,3,2,2,6,2,14,0,1,6,7,3,7,3,6,1,3,17,3,1,2,11,2,3,1];
+candidate1.electionResults = [5,1,7,2,17,6,4,2,1,1,8,3,1,11,11,0,5,3,3,3,7,4,8,9,3,7,2,2,4,2,8,3,15,15,2,12,0,4,13,1,3,2,8,11,3,2,11,1,3,7,2];
+candidate2.electionResults = [4,2,4,4,38,3,3,1,2,28,8,1,3,9,0,6,1,5,5,1,3,7,8,1,3,3,1,3,2,2,6,2,14,0,1,6,7,3,7,3,6,1,3,27,3,1,2,11,2,3,1];
 console.log(candidate1);
 console.log(candidate2);
 /* End My Stuff */
